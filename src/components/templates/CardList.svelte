df17492647e17c6ea178bd2b2f8cdbeac8972a59
Oliver Keane <ollyfudgey5@gmail.com>
2024-04-04 15:21:09 +0100

    Reduce opacity of related cards on hover for card lists

--- src/components/templates/CardList.svelte
+++ src/components/templates/CardList.svelte
@@ -18,22 +18,20 @@
 </script>
 
 {#if isProjectList(items)}
-    <FlexCol element="ol" className="gap-12 md:gap-4">
+    <FlexCol element="ol" className="gap-12 md:gap-4 [&_a:not(:hover)]:hover:opacity-50">
         {#each items.data as item}
             <ProjectCard {item}/>
         {/each}
     </FlexCol>
 {:else if isExperienceList(items)}
-    <FlexCol element="ul" className="gap-12 md:gap-4">
+    <FlexCol element="ul" className="gap-12 md:gap-4 [&_a:not(:hover)]:hover:opacity-50">
         {#each items.data as item}
             <ExperienceCard {item}/>
-
-            {#if item === (items.data.at(-1))}
-                <Link href="/resume.pdf" className="md:pl-4 font-medium">
-                    View Full Résumé
-                    <InteractiveArrow/>
-                </Link>
-            {/if}
         {/each}
     </FlexCol>
+
+    <Link href="/resume.pdf" className="block md:pl-4 mt-12 font-medium">
+        View Full Résumé
+        <InteractiveArrow/>
+    </Link>
 {/if }
