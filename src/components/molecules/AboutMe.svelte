68f988c8c4a7ba60404f5e68340f27f59de3b9dd
Oliver Keane <ollyfudgey5@gmail.com>
2025-11-03 16:21:14 +0000

    chore: separate text

--- src/components/molecules/AboutMe.svelte
+++ src/components/molecules/AboutMe.svelte
@@ -17,7 +17,10 @@
         Whilst at <Link href="https://www.keyholding.com/">TKC</Link> I've been focused on moving sections of our older platform into a new, more modern, "V2" codebase, introducing modern technologies
         like <Link href="https://tanstack.com/query/">Tanstack Query</Link>, <Link href="https://vitest.dev/">Vitest</Link> and <Link href="https://biomejs.dev/">Biome</Link>. 
         I also established a process to document architectural decisions through the use of RFCs and Architectural Decision Records. 
-        More recently, I've been working closely with senior business stakeholders and the product team, to develop technical proofs-of-concept and MVPs to help shape our architecture for future projects.
+    </Paragraph>
+
+    <Paragraph>
+        More recently, in my current role, I've been working closely with senior business stakeholders and the product team, to develop technical proofs-of-concept and MVPs to help shape our architecture for future projects.
     </Paragraph>
 
     <Paragraph>
